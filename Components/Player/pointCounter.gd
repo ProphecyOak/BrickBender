f5003fd0acbbec05f9cc5b108a881fa85881c05b
ProphecyOak <drito610@gmail.com>
2024-11-10 10:29:09 -0500

    LETS GO GAMBLING

--- Components/Player/pointCounter.gd
+++ Components/Player/pointCounter.gd
@@ -4,8 +4,9 @@ var oldPoints = 0
 
 func changePoints(player: PlayerCharacter):
 	if oldPoints == player.points: return
-	$Icons/MinusFive.visible = player.points < oldPoints
-	$Icons/PlusTen.visible = player.points > oldPoints
+	if $Icons.visible:
+		$Icons/MinusFive.visible = player.points < oldPoints
+		$Icons/PlusTen.visible = player.points > oldPoints
 	$AnimationPlayer.play("points")
 	oldPoints = player.points
 	text = str(oldPoints)
